c2fd4e4722019d049c10f13637a3c7c4ccf31c65
The Great Carstini <thegreatcarstini@Thes-MacBook-Air-3.local>
2025-05-06 14:58:10 -0400

    Final version

---
+++ docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME=mg-verilog
+CONTAINER_NAME=mg-verilog
+
+CONTAINER=$(docker container ls -a --filter name="^${CONTAINER_NAME}$" --format "{{.Names}}")
+if [ "$CONTAINER" != "$CONTAINER_NAME" ]; then
+    echo "Creating new container"
+    docker create -it \
+    -v $(pwd)/mg-verilog:/mg-verilog \
+    -w /mg-verilog \
+    --name ${CONTAINER_NAME} ${IMAGE_NAME}
+    docker start -a ${CONTAINER_NAME}
+fi
+
+# if container isn't running, start it
+STATUS=$(docker container ls --filter name=${CONTAINER_NAME} --format "{{.Names}}")
+if [ "$STATUS" != "$CONTAINER_NAME" ]; then
+    echo "Spinning up $CONTAINER_NAME"
+    docker start -a ${CONTAINER_NAME}
+else
+    echo "Connecting to new terminal"
+    docker exec -it ${CONTAINER_NAME} bash
+fi
